ffdc99d0790dd771409f29e148eace4a4e2ff07a
Viktor Nagy <viktor.nagy@01people.com>
2022-10-13 07:39:07 +0200

    feat: rewrite using hashmaps

--- lua/boole.lua
+++ lua/boole.lua
@@ -1,150 +1,152 @@
 local M = {}
 
-M.run = function(direction)
-  local under_cursor = vim.fn.expand('<cword>')
+M.replace_map = {
+    increment = {},
+    decrement = {},
+}
 
-  local change_word = function(word1, word2)
-    local match       = word1
-    local replacement = word2
+local generate_hashmaps = function(from, to)
+    M.replace_map.increment[from] = to
+    M.replace_map.decrement[to] = from
+end
+
+-- Proper booleans
+generate_hashmaps('true', 'false')
+generate_hashmaps('false', 'true')
+generate_hashmaps('yes', 'no')
+generate_hashmaps('no', 'yes')
+generate_hashmaps('on', 'off')
+generate_hashmaps('off', 'on')
+
+-- Days of the week
+generate_hashmaps('monday', 'tuesday')
+generate_hashmaps('tuesday', 'wednesday')
+generate_hashmaps('wednesday', 'thursday')
+generate_hashmaps('thursday', 'friday')
+generate_hashmaps('friday', 'saturday')
+generate_hashmaps('saturday', 'sunday')
+generate_hashmaps('sunday', 'monday')
+
+generate_hashmaps('Monday', 'Tuesday')
+generate_hashmaps('Tuesday', 'Wednesday')
+generate_hashmaps('Wednesday', 'Thursday')
+generate_hashmaps('Thursday', 'Friday')
+generate_hashmaps('Friday', 'Saturday')
+generate_hashmaps('Saturday', 'Sunday')
+generate_hashmaps('Sunday', 'Monday')
+
+generate_hashmaps('mon', 'tue')
+generate_hashmaps('tue', 'wed')
+generate_hashmaps('wed', 'thu')
+generate_hashmaps('thu', 'fri')
+generate_hashmaps('fri', 'sat')
+generate_hashmaps('sat', 'sun')
+generate_hashmaps('sun', 'mon')
+
+generate_hashmaps('Mon', 'Tue')
+generate_hashmaps('Tue', 'Wed')
+generate_hashmaps('Wed', 'Thu')
+generate_hashmaps('Thu', 'Fri')
+generate_hashmaps('Fri', 'Sat')
+generate_hashmaps('Sat', 'Sun')
+generate_hashmaps('Sun', 'Mon')
+
+-- Months of the year
+generate_hashmaps('january', 'february')
+generate_hashmaps('february', 'march')
+generate_hashmaps('march', 'april')
+generate_hashmaps('april', 'may')
+generate_hashmaps('may', 'june')
+generate_hashmaps('june', 'july')
+generate_hashmaps('july', 'august')
+generate_hashmaps('august', 'september')
+generate_hashmaps('september', 'october')
+generate_hashmaps('october', 'november')
+generate_hashmaps('november', 'december')
+generate_hashmaps('december', 'january')
+
+generate_hashmaps('January', 'February')
+generate_hashmaps('February', 'March')
+generate_hashmaps('March', 'April')
+generate_hashmaps('April', 'May')
+generate_hashmaps('May', 'June')
+generate_hashmaps('June', 'July')
+generate_hashmaps('July', 'August')
+generate_hashmaps('August', 'September')
+generate_hashmaps('September', 'October')
+generate_hashmaps('October', 'November')
+generate_hashmaps('November', 'December')
+generate_hashmaps('December', 'January')
+
+-- Colors
+generate_hashmaps('red', 'orange')
+generate_hashmaps('orange', 'yellow')
+generate_hashmaps('yellow', 'green')
+generate_hashmaps('green', 'blue')
+generate_hashmaps('blue', 'indigo')
+generate_hashmaps('indigo', 'violet')
+generate_hashmaps('violet', 'red')
+
+generate_hashmaps('Red', 'Orange')
+generate_hashmaps('Orange', 'Yellow')
+generate_hashmaps('Yellow', 'Green')
+generate_hashmaps('Green', 'Blue')
+generate_hashmaps('Blue', 'Indigo')
+generate_hashmaps('Indigo', 'Violet')
+generate_hashmaps('Violet', 'Red')
+
+M.run = function(direction)
+    local under_cursor = vim.fn.expand('<cword>')
+
+    local match = direction == 'decrement' and M.replace_map.decrement[under_cursor] or
+        M.replace_map.increment[under_cursor]
+
+    if match ~= nil then
+        return vim.cmd(':normal ciw' .. match)
+    end
+
+    -- <C-a> and <C-x> compatability
+    if direction == 'increment' then
+        if type(tonumber(under_cursor)) == 'number' then
+            return vim.cmd(':normal!')
+        end
+    end
 
     if direction == 'decrement' then
-      match       = word2
-      replacement = word1
+        if type(tonumber(under_cursor)) == 'number' then
+            return vim.cmd(':normal!')
+        end
     end
 
-    if under_cursor == match then
-      return vim.cmd(':normal ciw' .. replacement)
-    end
-  end
-
-  -- Proper booleans
-  change_word('true',  'false')
-  change_word('false', 'true')
-  change_word('yes',   'no')
-  change_word('no',    'yes')
-  change_word('on',    'off')
-  change_word('off',   'on')
-
-  -- Days of the week
-  change_word('monday',    'tuesday')
-  change_word('tuesday',   'wednesday')
-  change_word('wednesday', 'thursday')
-  change_word('thursday',  'friday')
-  change_word('friday',    'saturday')
-  change_word('saturday',  'sunday')
-  change_word('sunday',    'monday')
-
-  change_word('Monday',    'Tuesday')
-  change_word('Tuesday',   'Wednesday')
-  change_word('Wednesday', 'Thursday')
-  change_word('Thursday',  'Friday')
-  change_word('Friday',    'Saturday')
-  change_word('Saturday',  'Sunday')
-  change_word('Sunday',    'Monday')
-
-  change_word('mon', 'tue')
-  change_word('tue', 'wed')
-  change_word('wed', 'thu')
-  change_word('thu', 'fri')
-  change_word('fri', 'sat')
-  change_word('sat', 'sun')
-  change_word('sun', 'mon')
-
-  change_word('Mon', 'Tue')
-  change_word('Tue', 'Wed')
-  change_word('Wed', 'Thu')
-  change_word('Thu', 'Fri')
-  change_word('Fri', 'Sat')
-  change_word('Sat', 'Sun')
-  change_word('Sun', 'Mon')
-
-  -- Months of the year
-  change_word('january',   'february')
-  change_word('february',  'march')
-  change_word('march',     'april')
-  change_word('april',     'may')
-  change_word('may',       'june')
-  change_word('june',      'july')
-  change_word('july',      'august')
-  change_word('august',    'september')
-  change_word('september', 'october')
-  change_word('october',   'november')
-  change_word('november',  'december')
-  change_word('december',  'january')
-
-  change_word('January',   'February')
-  change_word('February',  'March')
-  change_word('March',     'April')
-  change_word('April',     'May')
-  change_word('May',       'June')
-  change_word('June',      'July')
-  change_word('July',      'August')
-  change_word('August',    'September')
-  change_word('September', 'October')
-  change_word('October',   'November')
-  change_word('November',  'December')
-  change_word('December',  'January')
-
-  -- Colors
-  change_word('red',    'orange')
-  change_word('orange', 'yellow')
-  change_word('yellow', 'green')
-  change_word('green',  'blue')
-  change_word('blue',   'indigo')
-  change_word('indigo', 'violet')
-  change_word('violet', 'red')
-
-  change_word('Red',    'Orange')
-  change_word('Orange', 'Yellow')
-  change_word('Yellow', 'Green')
-  change_word('Green',  'Blue')
-  change_word('Blue',   'Indigo')
-  change_word('Indigo', 'Violet')
-  change_word('Violet', 'Red')
-
-  -- <C-a> and <C-x> compatability
-  if direction == 'increment' then
-    if type(tonumber(under_cursor)) == 'number' then
-      return vim.cmd(':normal!')
-    end
-  end
-
-  if direction == 'decrement' then
-    if type(tonumber(under_cursor)) == 'number' then
-      return vim.cmd(':normal!')
-    end
-  end
-
-  return 0
+    return 0
 end
 
 M.setup = function(options)
-  vim.api.nvim_create_user_command(
-    'Boole',
-    function(args) M.run(args.args) end,
-    {nargs = 1, complete = function() return {'increment', 'decrement'} end}
-  )
-
-  if options == nil then return 1 end
-
-  if options.mappings.increment ~= nil then
-    vim.keymap.set(
-      {'n', 'v'},
-      options.mappings.increment,
-      '<Cmd>Boole increment<CR>'
+    vim.api.nvim_create_user_command(
+        'Boole',
+        function(args) M.run(args.args) end,
+        { nargs = 1, complete = function() return { 'increment', 'decrement' } end }
     )
-  end
 
-  if options.mappings.decrement ~= nil then
-    vim.keymap.set(
-      {'n', 'v'},
-      options.mappings.decrement,
-      '<Cmd>Boole decrement<CR>'
-    )
-  end
+    if options == nil then return 1 end
 
-  return 1
+    if options.mappings.increment ~= nil then
+        vim.keymap.set(
+            { 'n', 'v' },
+            options.mappings.increment,
+            '<Cmd>Boole increment<CR>'
+        )
+    end
+
+    if options.mappings.decrement ~= nil then
+        vim.keymap.set(
+            { 'n', 'v' },
+            options.mappings.decrement,
+            '<Cmd>Boole decrement<CR>'
+        )
+    end
+
+    return 1
 end
 
 return M
-
