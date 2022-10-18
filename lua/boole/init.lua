51c9dae7f34381911f5cebc27730672b25a32cb4
Viktor Nagy <viktor.nagy@01people.com>
2022-10-18 09:33:17 +0200

    fix: duplicate capitalized letters

--- lua/boole/init.lua
+++ lua/boole/init.lua
@@ -26,9 +26,7 @@ end
 
 local letters = {
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'
+    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
 }
 
 for _, letter in ipairs(letters) do
